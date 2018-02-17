708cc161864c0a066bf0cc4818438efb6c873be5
Edmund <eman0088@gmail.com>
2018-02-17 00:26:48 -0700

    Update Create Item

---
+++ contracts/items/createItem.sol
@@ -0,0 +1,16 @@
+pragma solidity ^0.4.18
+
+import 'zeppelin-solidity/contracts/token/ERC721/ERC721Token.sol'
+
+contract mintItem is ERC721Token {
+
+  function mintItem(address _to, uint256 _tokenId) {
+    _mint(_to, _tokenId);
+ 
+  }
+
+  function _genTokenId(address _to) private returns (unit256) {
+    _
+    keccak256()
+  }
+}
