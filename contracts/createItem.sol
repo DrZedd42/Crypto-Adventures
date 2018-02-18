b5d4c79883a7035ec4426309f20aa8e0c50d2bee
Edmund <eman0088@gmail.com>
2018-02-18 01:47:28 -0700

    Fix undeclared types

--- contracts/createItem.sol
+++ contracts/createItem.sol
@@ -5,13 +5,13 @@ import "../node_modules/zeppelin-solidity/contracts/token/ERC721/ERC721Token.sol
 contract mintItem is ERC721Token {
 
   function mintItem(address _to) external {
-    _tokenId = _genTokenId(_to);
+    uint _tokenId = _genTokenId(_to);
     _mint(_to, _tokenId);
   }
 
   function _genTokenId(address _to) private returns (uint256) {
-    randomString = "StringBackwards";
-    uint rand = uint(keccak256(randomString));
+    string storage randomStr = "StringBackwards";
+    uint rand = uint(keccak256(randomStr));
     return rand;
   }
 }
